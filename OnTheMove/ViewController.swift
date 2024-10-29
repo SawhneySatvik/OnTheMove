949e0516b854365c01cd98ba6c69710455c12972
SawhneySatvik <satvik.sawhney2005@gmail.com>
2024-10-29 09:05:27 +0530

    Initial Commit

---
+++ OnTheMove/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMove
+//
+//  Created by user@53 on 29/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
