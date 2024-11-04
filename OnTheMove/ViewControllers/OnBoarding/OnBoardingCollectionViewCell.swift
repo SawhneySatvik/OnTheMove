a281eb4b726d5386b2908a4d4ae1371c27a0938b
SawhneySatvik <satvik.sawhney2005@gmail.com>
2024-11-04 18:56:59 +0530

    On Boarding - except Button

---
+++ OnTheMove/ViewControllers/OnBoarding/OnBoardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnBoardingCollectionViewCell.swift
+//  OnTheMove
+//
+//  Created by user@53 on 29/10/24.
+//
+
+import UIKit
+
+class OnBoardingCollectionViewCell: UICollectionViewCell {
+    
+}
